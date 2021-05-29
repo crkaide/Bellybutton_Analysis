dded794ed22ab3136d54283b9d501ff00bf4ae76
crkaide <crkaide@ilstu.edu>
2021-05-28 23:51:13 -0500

    test

--- charts.js
+++ charts.js
@@ -1,24 +1,55 @@
+// Dynamically generates dropdown menu items
+
 function init() {
-  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
+    console.log(data);
     var sampleNames = data.names;
-
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
     });
+})}
 
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
-}
+init();
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function init() {
+//   // Grab a reference to the dropdown select element
+//   var selector = d3.select("#selDataset");
+
+//   // Use the list of sample names to populate the select options
+//   d3.json("samples.json").then((data) => {
+//     var sampleNames = data.names;
+
+//     sampleNames.forEach((sample) => {
+//       selector
+//         .append("option")
+//         .text(sample)
+//         .property("value", sample);
+//     });
+
+//     // Use the first sample from the list to build the initial plots
+//     var firstSample = sampleNames[0];
+//     buildCharts(firstSample);
+//     buildMetadata(firstSample);
+//   });
+// }
 
 // Initialize the dashboard
 init();
